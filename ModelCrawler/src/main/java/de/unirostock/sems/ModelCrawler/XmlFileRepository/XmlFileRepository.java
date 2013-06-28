0a657aeef5f20324b9ebd41b8d1d8697a5d22ed5
mp487 <martin.peters4@uni-rostock.de>
2013-06-28 15:26:23 +0200

    - adding model URI presets to main.properties - begin implementation XmlFileServer in XmlFileRepository

--- ModelCrawler/src/main/java/de/unirostock/sems/ModelCrawler/XmlFileRepository/XmlFileRepository.java
+++ ModelCrawler/src/main/java/de/unirostock/sems/ModelCrawler/XmlFileRepository/XmlFileRepository.java
@@ -1,15 +1,18 @@
 package de.unirostock.sems.ModelCrawler.XmlFileRepository;
 
 import java.io.File;
+import java.io.InputStream;
+import java.net.URI;
 import java.text.MessageFormat;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import de.unirostock.sems.ModelCrawler.Properties;
+import de.unirostock.sems.ModelCrawler.XmlFileRepository.Interface.XmlFileServer;
 import de.unirostock.sems.ModelCrawler.databases.BioModelsDb.BioModelsDb;
 
-public class XmlFileRepository {
+public class XmlFileRepository implements XmlFileServer {
 	
 	private static XmlFileRepository xmlFileRepository = null;
 	
@@ -55,10 +58,69 @@ public class XmlFileRepository {
 		}
 		
 		if( log.isInfoEnabled() )
-			log.info( MessageFormat.format("Openining  XmlFileRepository at {0}", location.getAbsolutePath()) );
+			log.info( MessageFormat.format("Opening  XmlFileRepository at {0}", location.getAbsolutePath()) );
 		
+	}
+	
+	/**
+	 * Checks if the URI is resolvable by the FileServer. <br>
+	 * (Tests scheme and host)
+	 * 
+	 * @param model
+	 * @return
+	 */
+	public boolean isResolvableUri(URI model) {
 		
+		if( !model.getScheme().equals( Properties.getProperty("de.unirostock.sems.ModelCrawler.models.uri.scheme") ))
+			return false;
 		
+		if( !model.getHost().equals( Properties.getProperty("de.unirostock.sems.ModelCrawler.models.uri.host") ))
+			return false;
+		
+		return true;
+	}
+	
+	@Override
+	public InputStream resolveModelUri(URI model) {
+		
+		if( model.isAbsolute() != true )
+			return null;
+		
+		if( isResolvableUri(model) != true )
+			return null;
+		
+		String path = model.getPath();
+		// matches the path against modelId/versionId scheme
+		if( path.matches("[a-BA-B0-9\\-\\.\\_]*\\/[a-BA-B0-9\\-\\.\\_]*") != true )
+			return null;
+		
+		File modelPath = new File( location, model.getPath() );
+		
+		// TODO
+		
+		return null;
+	}
+
+	@Override
+	public boolean exist(URI model) {
+		if( getModelPath(model).exists() )
+			return true;
+		else
+			return false;
+	}
+
+	@Override
+	public URI pushModel(String modelId, String versionId,
+			InputStream modelSource) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private File getModelPath(URI model) {
+		
+		// TODO
+		
+		return null;
 	}
 
 }
