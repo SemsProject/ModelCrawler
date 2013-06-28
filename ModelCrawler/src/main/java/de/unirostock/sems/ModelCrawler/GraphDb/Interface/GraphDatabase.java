901a2af77128f3f94880b4e325f662b8d8400f24
mp487 <martin.peters4@uni-rostock.de>
2013-06-28 10:18:21 +0200

    - moving GraphDb Interface to sub-package Interface 	+ extending - adding json-simple 1.1 to the dependencies - introducing QueryResult

--- ModelCrawler/src/main/java/de/unirostock/sems/ModelCrawler/GraphDb/Interface/GraphDatabase.java
+++ ModelCrawler/src/main/java/de/unirostock/sems/ModelCrawler/GraphDb/Interface/GraphDatabase.java
@@ -1,4 +1,6 @@
-package de.unirostock.sems.ModelCrawler.GraphDb;
+package de.unirostock.sems.ModelCrawler.GraphDb.Interface;
+
+import de.unirostock.sems.ModelCrawler.GraphDb.QueryResult;
 
 public interface GraphDatabase {
 	
@@ -23,6 +25,14 @@ public interface GraphDatabase {
 	 */
 	public String[] cellMlModelQueryFeatures();
 	
+	/**
+	 * Requests a single Model by its ModelId
+	 * 
+	 * 
+	 * @param modelId
+	 * @return QueryResult or NULL if no match found
+	 */
+	public QueryResult getCellMlModelFromId( String modelId );
 	
 	
 }
---
+++ ModelCrawler/src/main/java/de/unirostock/sems/ModelCrawler/GraphDb/QueryResult.java
@@ -0,0 +1,20 @@
+package de.unirostock.sems.ModelCrawler.GraphDb;
+
+import java.net.URI;
+
+public class QueryResult {
+	
+	private float score;
+	private String modelId;
+	private long databaseId;
+	
+	private String modelName;
+	
+	private URI documentUri;
+	private String filename;
+	
+	public QueryResult() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
