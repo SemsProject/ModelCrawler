71f819270932fde4416d782e465eb1d21c8e0acb
mp487 <martin.peters4@uni-rostock.de>
2013-06-28 14:37:20 +0200

    - specified methods for XmlFileServer (aka. XmlFileRepository)

--- ModelCrawler/src/main/java/de/unirostock/sems/ModelCrawler/XmlFileRepository/Interface/XmlFileServer.java
+++ ModelCrawler/src/main/java/de/unirostock/sems/ModelCrawler/XmlFileRepository/Interface/XmlFileServer.java
@@ -1,7 +1,35 @@
 package de.unirostock.sems.ModelCrawler.XmlFileRepository.Interface;
 
+import java.io.InputStream;
+import java.net.URI;
+
 public interface XmlFileServer {
 	
+	/**
+	 * Resolves the model URI and returns the modelSource
+	 * 
+	 * @param model URI
+	 * @return InputStream or {@code NULL} if URI not found
+	 */
+	public InputStream resolveModelUri( URI model );
 	
+	/**
+	 * Checks if the model referenced by the URI exits and is available
+	 * 
+	 * @param model
+	 * @return boolean
+	 */
+	public boolean exist( URI model );
+	
+	/**
+	 * Pushes a new model (-Version) into the storage. <br>
+	 * Returns the URI the new model or {@code null} if failed.
+	 * 
+	 * @param modelId
+	 * @param versionId
+	 * @param modelSource
+	 * @return URI to the new model
+	 */
+	public URI pushModel( String modelId, String versionId, InputStream modelSource );
 	
 }
