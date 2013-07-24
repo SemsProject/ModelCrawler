48ce5fd9777db7a02b795b5ae5c0eabe6fa4558b
mp487 <martin.peters4@uni-rostock.de>
2013-07-24 15:10:52 +0200

    - added dependency javahg (self compiled) - added class for PMR2 Database interaction

---
+++ ModelCrawler/src/main/java/de/unirostock/sems/ModelCrawler/databases/PMR2/Pmr2Db.java
@@ -0,0 +1,45 @@
+package de.unirostock.sems.ModelCrawler.databases.PMR2;
+
+import java.util.List;
+import java.util.Map;
+
+import de.unirostock.sems.ModelCrawler.databases.Interface.ChangeSet;
+import de.unirostock.sems.ModelCrawler.databases.Interface.ModelDatabase;
+
+public class Pmr2Db implements ModelDatabase {
+
+	public Pmr2Db() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<String> listModels() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, ChangeSet> listChanges() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ChangeSet getModelChanges(String modelId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void cleanUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
