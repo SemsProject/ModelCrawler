fe91eaf45b70fd0149af47b0def20b5ec4e1d689
mp487 <martin.peters3@uni-rostock.de>
2015-03-31 14:31:43 +0200

    auto distinguish model database

--- ModelCrawler/src/main/java/de/unirostock/sems/ModelCrawler/databases/Interface/ModelDatabase.java
+++ ModelCrawler/src/main/java/de/unirostock/sems/ModelCrawler/databases/Interface/ModelDatabase.java
@@ -4,7 +4,35 @@ import java.io.Closeable;
 import java.util.List;
 import java.util.Map;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import de.unirostock.sems.ModelCrawler.databases.BioModelsDb.BioModelsDb;
+import de.unirostock.sems.ModelCrawler.databases.PMR2.PmrDb;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@JsonTypeInfo(
+		use = JsonTypeInfo.Id.NAME,
+		include = JsonTypeInfo.As.PROPERTY,
+		property = "type" )
+@JsonSubTypes({
+	@Type( value = ModelDatabase.class, name = ModelDatabase.DatabaseTypes.NONE ),
+	@Type( value = BioModelsDb.class, name = ModelDatabase.DatabaseTypes.BMDB ),
+	@Type( value = PmrDb.class, name = ModelDatabase.DatabaseTypes.PMR2 )
+})
 public abstract class ModelDatabase implements Runnable, Closeable {
+	
+	public abstract class DatabaseTypes {
+		public static final String NONE = "";
+		public static final String BMDB = "BMDB";
+		public static final String PMR2 = "PMR2";
+	}
+	
+	private String type = DatabaseTypes.NONE;
 	
 	/**
 	 * lists all Models in the latest revision
@@ -40,4 +68,10 @@ public abstract class ModelDatabase implements Runnable, Closeable {
 	 */
 	@Override
 	public abstract void run();
+
+	
+	public String getType() {
+		return type;
+	}
+	
 }
